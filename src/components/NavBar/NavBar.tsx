414af217149328363af697d13332524ca60ee093
Kevin Cuadros <kevindavidcuadros@gmail.com>
2022-01-17 10:59:52 -0500

    creando home page

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,51 +1,70 @@
-import Image from "next/image"
-import styles from "./NavBar.module.css"
-import Link from "next/link"
 import { useState } from "react"
+import Image from "next/image"
+import Link from "next/link"
+import { AiOutlineMenu } from "react-icons/ai"
+import { ImCancelCircle } from "react-icons/im"
+import styles from "./NavBar.module.css"
 
 const NavBar = () => {
 
   const [menuMobile, setMenuMobile] = useState(false)
 
   return (
-    <header>
-      <nav className={styles.nav}>
-        <div>
-          <Image 
-            src={"/elephant-logo.png"}
-            alt="logo"
-            width={35}
-            height={35}
-            quality={100}
-          />
-        </div>
+    <>
+      <header className={styles.header}>
+        <nav className={styles.nav}>
+          <div>
+            <Image 
+              src={"/elephant-logo.png"}
+              alt="logo"
+              width={35}
+              height={35}
+            />
+          </div>
 
-        <div>
-          <ul className={styles.nav_list}>
+          <button 
+          className={`${styles.button_menu}`}
+          >
+            {
+              menuMobile 
+              ? <ImCancelCircle 
+                size={25} 
+                onClick={() => setMenuMobile(!menuMobile)}
+              />
+              : <AiOutlineMenu  
+                size={25} 
+                onClick={() => setMenuMobile(!menuMobile)} 
+              /> 
+            }
+          </button>
+
+          
+          
+        </nav>
+          <ul className={`${styles.nav_list} ${menuMobile ? styles.active : ""}`}>
             <li>
-              <Link href={""}>
+              <Link href={"/"}>
                 <a>Colaborar</a>
               </Link>
             </li>
             <li>
-              <Link href={""}>
+              <Link href={"/"}>
                 <a>Casos</a>
               </Link>
             </li>
             <li>
-              <Link href={""}>
+              <Link href={"/"}>
                 <a>Iniciar sesión</a>
               </Link>
             </li>
             <li>
-              <Link href={""}>
+              <Link href={"/"}>
                 <a className={styles.last_item}>Registrarse</a>
               </Link>
             </li>
           </ul>
-        </div>
-      </nav>
-    </header>
+      </header>
+    </>
   )
 }
 
