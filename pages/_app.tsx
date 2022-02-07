19f533c0a609ecdf72e43144f7cc6f870ced4ca0
Kevin Cuadros <kevindavidcuadros@gmail.com>
2022-02-07 08:43:11 -0500

    test fonts optimization

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,27 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { Layout } from '../src/components/index'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+     <Head>
+         {/* eslint-disable-next-line @next/next/no-css-tags */}
+        <link
+            rel="stylesheet"
+            href="/assets/Dongle-Regular.ttf"
+          />
+         {/* eslint-disable-next-line @next/next/no-css-tags */}
+          <link
+            rel="stylesheet"
+            href="/assets/Dongle-Bold.ttf"
+          />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   ) 
 }
 
