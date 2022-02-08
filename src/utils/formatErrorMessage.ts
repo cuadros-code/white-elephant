a1a434b4ba779af415a65514990a05c1165ed2a5
Kevin Cuadros <kevindavidcuadros@gmail.com>
2022-02-07 23:32:07 -0500

    format code errors

--- src/utils/formatErrorMessage.ts
+++ src/utils/formatErrorMessage.ts
@@ -6,6 +6,10 @@ export const formatErrorMessage = (error: string) => {
       return 'El correo no es valido'
     case 'auth/operation-not-allowed':
       return 'No se puede crear el usuario'
+    case 'auth/usser-not-found':
+      return 'El usuario no existe'
+    case 'auth/wrong-password':
+      return 'El usuario o contraseña es incorrecta'
     default:
       return 'Error de autenticación'
   }
