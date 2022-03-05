f031de9863a21dd174c1306bb35e11362320ce23
Kevin Cuadros <kevindavidcuadros@gmail.com>
2022-03-04 23:42:57 -0500

    cambiando estilo de mapa

--- pages/dashboard/create.tsx
+++ pages/dashboard/create.tsx
@@ -3,39 +3,42 @@ import dynamic from 'next/dynamic';
 import { useForm } from 'react-hook-form';
 import LayoutDashboard from 'src/components/LayoutDasboard'
 import styles from 'styles/CreateComplaint.module.css'
-import { TextField } from 'src/components';
-const AutoCompleteInput = dynamic(
-    () => import("src/components/TextField/AutoComplete/AutoCompleteInput"), 
-    {
-       ssr:false,
-       loading: () => <div>Loading...</div>,
-    }
-)
+import { PrimaryButton, TextField } from 'src/components';
+const AutoCompleteInput = dynamic(() => import("src/components/TextField/AutoComplete/AutoCompleteInput"), { ssr:false})
 interface IFormCreate {
-  email   : string
-  password: string
+  email      : string
+  location   : string
+  lat: number
+  lng: number
 }
 
 const Create = () => {
 
-  const { register, handleSubmit, formState: { errors } } = useForm<IFormCreate>({
+  const { register, handleSubmit, formState: { errors }, setValue } = useForm<IFormCreate>({
     // resolver: yupResolver(schemaLogin)
   });
 
   const onSubmit = (data: IFormCreate) => {
-
+    console.log(data)
   };
 
   return (
     <div className={styles.container}>
       <form onSubmit={handleSubmit(onSubmit)} autoComplete='off'>
-          <AutoCompleteInput />
           <TextField
             {...register('email')} 
             label='Titulo de la denuncia'
             placeholder='Ingresa el titulo de la denuncia'
             error={errors.email}
           />
+          <AutoCompleteInput 
+            reference={{...register('location')}}
+            setValueForm={setValue}
+          />
+
+          <PrimaryButton  type='submit'>
+            Crear Denuncia
+          </PrimaryButton>
       </form>
     </div>
   )
--- src/components/TextField/AutoComplete/AutoCompleteInput.tsx
+++ src/components/TextField/AutoComplete/AutoCompleteInput.tsx
@@ -1,16 +1,22 @@
-import React, { useState } from 'react'
+import React, { DetailedHTMLProps, InputHTMLAttributes, useState } from 'react'
 import usePlacesService from "react-google-autocomplete/lib/usePlacesAutocompleteService";
 import { BiCurrentLocation } from 'react-icons/bi';
 import { TextField } from 'src/components';
 import { Map, Marker } from "pigeon-maps"
 import styles from './AutoComplete.module.css';
 import { useMessageError } from 'src/store/messageStore';
+import { UseFormSetValue } from 'react-hook-form';
 
-const AutoCompleteInput = () => {
-  
+interface TextFieldProps extends DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>{
+  error?    : boolean;
+  reference?: any;
+  setValueForm  : UseFormSetValue<any>
+}
+
+const AutoCompleteInput = (props: TextFieldProps) => {
+  const { error, children, reference, setValueForm, ...rest } = props;
   const message = useMessageError( state => state );
   const [closeList, setCloseList] = useState(false);
-  const [value, setValue] = useState("")
   const [locationState, setLocationState] = useState<[number, number]>([3.3658895, -76.5950555])
   
   const {
@@ -26,6 +32,7 @@ const AutoCompleteInput = () => {
   const getLocation = () => {
     navigator.geolocation.getCurrentPosition(( data: GeolocationPosition ) => {
       setLocationState([data.coords.latitude, data.coords.longitude]);
+      sendCoordinates(data.coords.latitude, data.coords.longitude);
     }, () => {
       message.setError({
         error: true,
@@ -33,7 +40,20 @@ const AutoCompleteInput = () => {
         type: 'warning'
       })
     });
-    
+  }
+
+  const onClickItemLocation = (item: { place_id: string, description: string }) => {
+    placesService.getDetails({ placeId: item.place_id }, (data: any) => {
+      setLocationState([data.geometry.location.lat(), data.geometry.location.lng()]);
+      sendCoordinates(data.geometry.location.lat(), data.geometry.location.lng());
+    });
+    setValueForm('location', item.description);
+    setCloseList(true);
+  }
+
+  const sendCoordinates = (lat: number, lng: number) => {
+    setValueForm('lat', lat);
+    setValueForm('lng', lng);
   }
 
   return (
@@ -41,13 +61,12 @@ const AutoCompleteInput = () => {
       <div className={styles.contentLocation}>
         <div>
           <TextField
-            value={value}
+            {...reference}
             loading={isPlacePredictionsLoading}
             label='Dirección'
             placeholder='Ingresa tu dirección'
             onChange={(evt: any) => {
               getPlacePredictions({ input: evt.target.value });
-              setValue(evt.target.value);
               setCloseList(false);
             }}
           />
@@ -55,35 +74,33 @@ const AutoCompleteInput = () => {
         <button 
           className={styles.buttonLocation}
           onClick={getLocation}
+          type='button'
         >
             <BiCurrentLocation size={25} />
         </button>
       </div>
+
       <div className={styles.containerList}>
         { !closeList && placePredictions.map((item, index) => (
             <li 
               className={styles.list} 
               key={index} 
-              onClick={() => { 
-                placesService.getDetails({ placeId: item.place_id }, (data: any) => {
-                  setLocationState([data.geometry.location.lat(), data.geometry.location.lng()]);
-                });
-                setValue(item.description);
-                setCloseList(true);
-              }}
+              onClick={() => onClickItemLocation(item)}
             >
               {item.description}
             </li>
         ))}
       </div>
 
-      <Map 
-        height={300}
+      
+      <Map
+        boxClassname={styles.boxLocation}
+        height={250}
         defaultCenter={locationState} 
         center={locationState}
         defaultZoom={11}
-      >
-        <Marker width={50} anchor={locationState} />
+        >
+        <Marker width={50} anchor={locationState} color='#005cd4'/>
       </Map>
     </div>
   )
